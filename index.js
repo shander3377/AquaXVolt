f1e76deaafac39edae5bcc7b76934eb06e42e8d1
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-06-26 16:19:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const ms = require('ms');
 
 const token = process.env.token;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`App is running on port ${ PORT }`);
-});
-
 const PREFIX = '=';
 
 bot.on('ready', () => {
