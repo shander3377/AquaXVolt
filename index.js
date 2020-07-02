b8456824aa567ad142c2c56a50b1e3bf578e84c6
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-07-02 18:21:37 +0530

    test

--- index.js
+++ index.js
@@ -182,7 +182,74 @@ bot.on('message', message => {
 
             break;
     }
-})
+});
+
+bot.on('message', message => {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+    if (!message.content.startsWith(PREFIX)) return;
+
+    switch (args[0]) {
+        case 'mute':
+            if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+                const botnopermissionmuteembed = new Discord.RichEmbed()
+                botnopermissionmuteembed.setColor(0xFF0000)
+                botnopermissionmuteembed.setDescription("I don't have Ban Members Permission.")
+                return message.channel.send(botnopermissionmuteembed)
+            }
+
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+                const nopermissionmuteembed = new Discord.RichEmbed()
+                nopermissionmuteembed.setColor(0xFF0000)
+                nopermissionmuteembed.setDescription("❌ You do not have permissions to mute members. Please contact a staff member")
+                return message.channel.send(nopermissionmuteembed)
+            }
+
+            let person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+            let reason = args[2]
+
+            let muterole = message.guild.roles.find(role => role.name === 'Muted');
+
+            if (!muterole) return message.reply("Couldn't find the mute role");
+
+            if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+                const botnopermission_manage_rolesembed = new Discord.RichEmbed()
+                botnopermission_manage_rolesembed.setColor(0xFF0000)
+                botnopermission_manage_rolesembed.setDescription("I don't have Manage Roles Permission.")
+                return message.channel.send(botnopermission_manage_rolesembed)
+            }
+
+            let time = args[1];
+
+            if (!time) {
+                return message.reply("You didn't specify a time!");
+            }
+
+            if (!reason) {
+                return message.reply("You didn't specify a reason!");
+            }
+            person.addRole(muterole.id);
+            const muteembed = new Discord.RichEmbed()
+            muteembed.setColor(0x00FFFF)
+            muteembed.setDescription(`✅ **${person.user.tag} has been muted for ${ms(ms(time))} because ${reason}**`);
+            message.channel.send(muteembed);
+
+            setTimeout(function () {
+                person.removeRole(muterole.id);
+                const unmuteembed = new Discord.RichEmbed()
+                unmuteembed.setColor(0x00FFFF)
+                unmuteembed.setDescription(`✅ **${person.user.tag} has been unmuted!**`)
+                message.channel.send(unmuteembed)
+
+            }, ms(time));
+
+
+            break;
+    }
+
+});
+
 
 bot.login(token);
 
