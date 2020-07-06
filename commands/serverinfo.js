ca6def256184481d2bc19acf825a223660d617c6
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-07-06 19:32:06 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,30 +9,30 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send(embed)
     }
 
-  const verlvl = {
-    0: "None",
-    1: "Low",
-    2: "Medium",
-    3: "(╯°□°）╯︵ ┻━┻",
-    4: "(ノಠ益ಠ)ノ彡┻━┻"
-  }
+    const verlvl = {
+        0: "None",
+        1: "Low",
+        2: "Medium",
+        3: "(╯°□°）╯︵ ┻━┻",
+        4: "(ノಠ益ಠ)ノ彡┻━┻"
+    }
 
     let inline = true
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setColor("#00ff00")
-    .setThumbnail(sicon)
-    .setAuthor(message.guild.name)
-    .addField("Name", message.guild.name, inline)
-    .addField("ID", message.guild.id, inline)
-    .addField("Owner", message.guild.owner, inline)
-    .addField("Region", message.guild.region, inline)
-    .addField("Verification Level", verlvl[message.guild.verificationLevel],inline)
-    .addField("Members", `${message.guild.memberCount}`, inline)
-    .addField("Roles", message.guild.roles.size, inline)
-    .addField("Channels", message.guild.channels.size, inline)
-    .addField("You Joined", message.member.joinedAt)
-    .setFooter(`Created ${message.guild.createdAt}`);
+        .setColor("#00ff00")
+        .setThumbnail(sicon)
+        .setAuthor(message.guild.name)
+        .addField("Name", message.guild.name, inline)
+        .addField("ID", message.guild.id, inline)
+        .addField("Owner", message.guild.owner, inline)
+        .addField("Region", message.guild.region, inline)
+        .addField("Verification Level", verlvl[message.guild.verificationLevel], inline)
+        .addField("Members", `${message.guild.memberCount}`, inline)
+        .addField("Roles", message.guild.roles.size, inline)
+        .addField("Channels", message.guild.channels.size, inline)
+        .addField("You Joined", message.member.joinedAt)
+        .setFooter(`Created ${message.guild.createdAt}`);
 
     message.channel.send(serverembed);
 
@@ -40,5 +40,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name:"serverinfo"
+    name: "serverinfo"
 }
