3ae0f1a02ac4e859e5ad7adf9f03cbf26e56d887
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-07-06 19:39:04 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,43 +2,43 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_SERVER")) {
-        const embed = new Discord.RichEmbed()
-        embed.setColor(0xFF0000)
-        embed.setDescription("❌ You do not have permissions to check server info. Please contact a staff member")
-        return message.channel.send(embed)
-    }
+  if (!message.member.hasPermission("MANAGE_GUILD")) {
+    const embed = new Discord.RichEmbed()
+    embed.setColor(0xFF0000)
+    embed.setDescription("❌ You do not have permissions to check server info. Please contact a staff member")
+    return message.channel.send(embed)
+  }
 
-    const verlvl = {
-        0: "None",
-        1: "Low",
-        2: "Medium",
-        3: "(╯°□°）╯︵ ┻━┻",
-        4: "(ノಠ益ಠ)ノ彡┻━┻"
-    }
+  const verlvl = {
+    0: "None",
+    1: "Low",
+    2: "Medium",
+    3: "(╯°□°）╯︵ ┻━┻",
+    4: "(ノಠ益ಠ)ノ彡┻━┻"
+  }
 
-    let inline = true
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-        .setColor("#00ff00")
-        .setThumbnail(sicon)
-        .setAuthor(message.guild.name)
-        .addField("Name", message.guild.name, inline)
-        .addField("ID", message.guild.id, inline)
-        .addField("Owner", message.guild.owner, inline)
-        .addField("Region", message.guild.region, inline)
-        .addField("Verification Level", verlvl[message.guild.verificationLevel], inline)
-        .addField("Members", `${message.guild.memberCount}`, inline)
-        .addField("Roles", message.guild.roles.size, inline)
-        .addField("Channels", message.guild.channels.size, inline)
-        .addField("You Joined", message.member.joinedAt)
-        .setFooter(`Created ${message.guild.createdAt}`);
+  let inline = true
+  let sicon = message.guild.iconURL;
+  let serverembed = new Discord.RichEmbed()
+    .setColor("#00FFFF")
+    .setThumbnail(sicon)
+    .setAuthor(message.guild.name)
+    .addField("Name", message.guild.name, inline)
+    .addField("ID", message.guild.id, inline)
+    .addField("Owner", message.guild.owner, inline)
+    .addField("Region", message.guild.region, inline)
+    .addField("Verification Level", verlvl[message.guild.verificationLevel], inline)
+    .addField("Members", `${message.guild.memberCount}`, inline)
+    .addField("Roles", message.guild.roles.size, inline)
+    .addField("Channels", message.guild.channels.size, inline)
+    .addField("You Joined", message.member.joinedAt)
+    .setFooter(`Created ${message.guild.createdAt}`);
 
-    message.channel.send(serverembed);
+  message.channel.send(serverembed);
 
-    message.delete();
+  message.delete();
 }
 
 module.exports.help = {
-    name: "serverinfo"
+  name: "serverinfo"
 }
