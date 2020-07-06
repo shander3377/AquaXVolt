8cc720f275f1cf40ea218b382a0538b8c1920105
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-07-06 19:14:11 +0530

    hmm

---
+++ commands/ping.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const PREFIX = '=';
+
+module.exports.run = async (bot, message, args) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        var ping = Date.now() - message.createdTimestamp + " ms";
+        const m = await message.channel.send("ping?");
+        m.edit("PONG! `" + `${Date.now() - message.createdTimestamp}` + " ms`");
+
+    }
+}
+
+module.exports.help = {
+    name:"ping"
+  }
