4f9ae88845e66c65085d13eae849401dfe48509e
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2020-07-09 23:53:18 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,59 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("MANAGE_GUILD")) {
+        const embed = new Discord.RichEmbed()
+        embed.setColor(0xFF0000)
+        embed.setDescription("❌ You do not have permissions to check server info. Please contact a staff member[Missing Permission:- Manage Server]")
+        return message.channel.send(embed)
+    }
+
+    let helpArray = message.content.split(" ");
+    let helpArgs = helpArray.slice(1);
+
+    if (!helpArgs[0]) {
+        var embed = new Discord.RichEmbed()
+            .setAuthor(`Here is the Available Commands to use:`)
+            .setThumbnail(message.guild.iconURL)
+            .setTimestamp()
+            .setDescription("**Moderation**\n ``Clear`` | ``Mute`` | ``Unmute`` | ``Kick`` | ``Ban`` | ``CreateRole`` | ``Addrole`` | ``Removerole``\n **Info**\n``Ping`` | ``Info (Version/Creator)`` | ``Serverinfo`` | ``Userinfo`` \n**Fun**\n ``Avatar`` | ``Coinflip`` | ``Rps`` \n**Prefix (Default)**\n``=``")
+            .setFooter(message.author.username, message.author.displayAvatarURL)
+            .setColor('0x00FFF3')
+
+        message.channel.send(embed);
+    }
+
+    if (helpArgs[0]) {
+        let command = helpArgs[0];
+
+        if (bot.commands.has(command)) {
+            command = bot.commands.get(command);
+            var embed = new Discord.RichEmbed()
+                .setAuthor(`${command.help.name} Command`)
+                .setThumbnail(message.guild.iconURL)
+                .setTimestamp()
+                .setDescription(`
+            -> **Command's Description :- **__${command.help.description || "There is no Description for this command."}__
+            -> **Command's Usage :- ** __${command.help.usage || "No Usage"}__
+            -> **Command's Examples :- ** __${command.help.example || "No Examples"}__
+            -> **Command's Permissions :- ** __${command.help.accessableby || "Members"}__
+            -> **Command's Aliases :- ** __${command.help.aliases || "No Aliases"}__
+            `)
+                .setFooter(message.author.username, message.author.displayAvatarURL)
+                .setColor(`0x2EFF00`)
+
+            message.channel.send(embed);
+        }
+    }
+}
+
+
+module.exports.help = {
+    name: "help",
+    description: "",
+    usage: "?help",
+    accessableby: "Manage Server",
+    aliases: ["h", "commands"]
+}
